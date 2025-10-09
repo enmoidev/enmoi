7a84261c0647e97c401f0fe39bca5c96e64556d8
sebaDevTech <sebastienpetit.dev@gmail.com>
2025-10-09 23:52:10 +0200

    fix initial deploy

--- next.config.ts
+++ next.config.ts
@@ -17,11 +17,10 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' ${betterAuthURL};
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               font-src 'self' https://fonts.gstatic.com;
               img-src 'self' data: blob:; /* ${/* ${s3BaseURL} */ ""} */
-              connect-src 'self' ${betterAuthURL} https://fonts.googleapis.com https://fonts.gstatic.com; /* ${!isProd ? "http://localhost:3000" : ""} ${/* ${s3BaseURL} */ ""} */
+              connect-src *;
               object-src 'none';
               base-uri 'self';
             `.replace(/\s{2,}/g, " ").trim(),
