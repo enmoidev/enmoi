2906c69899455abc5bd525423c0196c94f7d57f0
sebaDevTech <sebastienpetit.dev@gmail.com>
2025-10-10 13:04:48 +0200

    fix initial deploy

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const betterAuthURL = process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "";
 const isProd = process.env.NODE_ENV === "production";
 
-// const awsRegion = process.env.AWS_REGION || "eu-west-3";
-// const awsBucket = process.env.AWS_S3_BUCKET_NAME || "";
-// const s3BaseURL = `https://${awsBucket}.s3.${awsRegion}.amazonaws.com`;
-
 const nextConfig: NextConfig = {
   async headers() {
     return [
@@ -20,8 +16,8 @@ const nextConfig: NextConfig = {
               script-src 'self' 'unsafe-inline' 'unsafe-eval' ${betterAuthURL};
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               font-src 'self' https://fonts.gstatic.com;
-              img-src 'self' data: blob:; /* ${/* ${s3BaseURL} */ ""} */
-              connect-src 'self' ${betterAuthURL} https://fonts.googleapis.com https://fonts.gstatic.com; /* ${!isProd ? "http://localhost:3000" : ""} ${/* ${s3BaseURL} */ ""} */
+              img-src 'self' data: blob:;
+              connect-src 'self' ${betterAuthURL} https://fonts.googleapis.com https://fonts.gstatic.com ${!isProd ? "http://localhost:3000" : ""};
               object-src 'none';
               base-uri 'self';
             `.replace(/\s{2,}/g, " ").trim(),
@@ -46,15 +42,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: `${awsBucket}.s3.${awsRegion}.amazonaws.com`,
-  //     },
-  //   ],
-  // },
-
 };
 
 export default nextConfig;
